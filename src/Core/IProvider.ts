495a3866c4395b8b4b586f13abf15110442fe5bc
Alexey Nemiro <alexey.nemiro@clearscale.com>
2021-08-07 12:50:44 +0300

    [Refactoring] IProvider - Removed owner and repo.
    This is for GitHub only and we can do without these properties.

--- src/Core/IProvider.ts
+++ src/Core/IProvider.ts
@@ -2,10 +2,6 @@ import { Branch, BranchListItem } from './Types';
 
 export interface IProvider {
 
-  readonly owner: string;
-
-  readonly repo: string;
-
   getListBranches(): Promise<Array<BranchListItem>>;
 
   getBranch(branchName: string, lastCommitHash?: string): Promise<Branch>;
