9d6070c6e309bb05387b1717674f303755d1f495
Alexey Nemiro <alexey.nemiro@clearscale.com>
2021-08-08 21:31:06 +0300

    Implemented IBranchRemover.

---
+++ src/BranchRemover.ts
@@ -0,0 +1,193 @@
+import chalk from 'chalk';
+
+import {
+  BranchRemoverContext,
+  BranchRemoverOptions,
+  BranchRemoverOptionsIgnoreArgs,
+  BranchRemoverOptionsIgnoreFunction,
+  BranchRemoverOptionsIgnoreType,
+  BranchRemoverOptionsRemoveArgs,
+  BranchRemoverOptionsRemoveFunction,
+  BranchRemoverOptionsRemoveType,
+  IBranchRemover,
+  IProvider,
+  Logger,
+} from './Core';
+
+export class BranchRemover implements IBranchRemover {
+
+  private readonly _provider: IProvider;
+
+  public get provider(): IProvider {
+    return this._provider;
+  }
+
+  constructor(provider: IProvider) {
+    this._provider = provider;
+
+    this.execute = this.execute.bind(this);
+  }
+
+  public async execute(options: BranchRemoverOptions, test?: boolean): Promise<void> {
+    const start = new Date();
+    const logger = options.logger || new Logger();
+    const ignore = this.buildIgnoreFunction(options.ignore);
+    const remove = this.buildRemoveFunction(options.remove);
+    const context: BranchRemoverContext = {
+      test,
+      logger,
+    };
+
+    logger.info(
+      'Processing branches using {provider} provider in {mode} mode.',
+      {
+        provider: chalk.yellow(this._provider.name),
+        mode: test ? chalk.yellow('test') : chalk.red('remove'),
+      }
+    );
+
+    const branches = await this._provider.getListBranches();
+
+    let totalRemoved = 0;
+
+    for (let i = 0, ic = branches.length; i < ic; ++i) {
+      const { name, lastCommitHash } = branches[i];
+
+      logger.info(
+        'Processing branch {index} of {count} - {branch}',
+        {
+          index: chalk.blue(i + 1),
+          count: chalk.blue(ic),
+          branch: name,
+        }
+      );
+
+      const ignored = await ignore({
+        branchName: name,
+        context,
+      });
+
+      if (ignored) {
+        logger.info(
+          'Skip branch "{branch}".',
+          {
+            branch: name,
+          }
+        );
+        continue;
+      }
+
+      const branch = await this._provider.getBranch(
+        name,
+        lastCommitHash
+      );
+
+      if (!branch) {
+        continue;
+      }
+
+      const canRemove = await remove({
+        branch,
+        context,
+      });
+
+      if (canRemove) {
+        logger.info(
+          'Removing "{branch}"...',
+          {
+            branch: name,
+          }
+        );
+
+        if (test) {
+          logger.info('Skipped in test mode.');
+        } else {
+          await this._provider.removeBranch(name);
+
+          ++totalRemoved;
+
+          logger.info(
+            'Successfully removed "{branch}"',
+            {
+              branch: name,
+            }
+          );
+        }
+      }
+    }
+
+    logger.info(
+      'Removed {totalRemoved} out of {totalCount} branches in {duration} seconds.',
+      {
+        totalRemoved,
+        totalCount: branches.length,
+        duration: (new Date().getTime() - start.getTime()) / 1000,
+      }
+    );
+  }
+
+  private buildIgnoreFunction(value: BranchRemoverOptionsIgnoreType): BranchRemoverOptionsIgnoreFunction {
+    if (typeof value === 'function') {
+      return value;
+    }
+
+    if (typeof value === 'string') {
+      return (e: BranchRemoverOptionsIgnoreArgs): Promise<boolean> => {
+        return Promise.resolve(
+          e.branchName === value
+        );
+      };
+    }
+
+    if (value instanceof RegExp) {
+      return (e: BranchRemoverOptionsIgnoreArgs): Promise<boolean> => {
+        return Promise.resolve(
+          value.test(e.branchName)
+        );
+      };
+    }
+
+    if (Array.isArray(value)) {
+      return (e: BranchRemoverOptionsIgnoreArgs): Promise<boolean> => {
+        return Promise.resolve(
+          value.includes(e.branchName)
+        );
+      };
+    }
+
+    return (): Promise<boolean> => Promise.resolve(false);
+  }
+
+  private buildRemoveFunction(value: BranchRemoverOptionsRemoveType): BranchRemoverOptionsRemoveFunction {
+    if (typeof value === 'function') {
+      return value;
+    }
+
+    if (typeof value === 'string') {
+      return ({ branch }: BranchRemoverOptionsRemoveArgs): Promise<boolean> => {
+        return Promise.resolve(
+          branch.name === value
+        );
+      };
+    }
+
+    if (value instanceof RegExp) {
+      return ({ branch }: BranchRemoverOptionsRemoveArgs): Promise<boolean> => {
+        return Promise.resolve(
+          value.test(branch.name)
+        );
+      };
+    }
+
+    if (Array.isArray(value)) {
+      return ({ branch }: BranchRemoverOptionsRemoveArgs): Promise<boolean> => {
+        return Promise.resolve(
+          value.includes(branch.name)
+        );
+      };
+    }
+
+    return ({ branch }): Promise<boolean> => Promise.resolve(branch.merged);
+  }
+
+}
