4faa493b77274a117bd3a9738ef7529bdf8fc4a9
Alexey Nemiro <alexey.nemiro@clearscale.com>
2021-08-15 18:17:20 +0300

    GetParams - Fixed bug with aliases.

--- src/CommandLine/GetParams.ts
+++ src/CommandLine/GetParams.ts
@@ -4,18 +4,22 @@ import { CommandLineParams } from './CommandLineParams';
 import { ParameterInfo } from './ParameterInfo';
 import { schema } from './Schema';
 
+const alias = Object.fromEntries(
+  schema.filter(
+    (x: ParameterInfo): boolean => !!x.aliases?.length
+  ).map(
+    (x: ParameterInfo): Array<string | Array<string>> => {
+      return [
+        x.name, x.aliases,
+      ];
+    }
+  )
+);
+
 export const params = minimist(
   process.argv,
   {
-    alias: schema.filter(
-      (x: ParameterInfo): boolean => !!x.aliases?.length
-    ).map(
-      (x: ParameterInfo): { [key: string]: Array<string> } => {
-        return {
-          [x.name]: x.aliases,
-        };
-      }
-    ),
+    alias,
     boolean: [
       'version',
       'test',
